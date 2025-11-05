a936c9c8ede45a049df851b9e044cc62fa682150
QING-XIAO <xiaoqing.zzz.me@gmail.com>
2025-11-05 15:10:10 +0800

    添加网站统计.

--- docs/.vitepress/ConfigHyde/Nav.ts
+++ docs/.vitepress/ConfigHyde/Nav.ts
@@ -83,6 +83,7 @@ export default [
         items: [
           {text: '⏳ 归档页', link: '/archives'},
           {text: '⚓ 标签页', link: '/tags'},
+          {text: '🌏 网站统计', link: 'https://cloud.umami.is/share/fSPKAWXYGAN1CVgT'},
         ]
       }
     ];
